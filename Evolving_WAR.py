44d1cc9bba2090f8b0014ffe55d9e08dc1e43b2e
Anthony Dalke <anthony.dalke@gmail.com>
2019-06-13 07:51:07 -0500

    Cleaned up variable names and merged df's

--- Evolving_WAR.py
+++ Evolving_WAR.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import os
 
-# Set working director
+# Set working directory
 os.getcwd()
 os.chdir('/Users/anthony/Documents/Hockey')
 
@@ -13,6 +13,7 @@ war = pd.read_csv("EvolvingWAR.csv")
 
 # Preview data
 war.columns
+war.head()
 war.info
 
 # Create pivot table of total WAR and games played by team, season and position
@@ -24,11 +25,12 @@ war_pivot = pd.pivot_table(data = war,
                      margins = True,
                      fill_value = 0)
 
-war_pivot_calc = pd.concat([war_pivot, 
+# Add calculation of percentage of total WAR contributed by defensemen
+war_df = pd.concat([war_pivot, 
                             war_pivot['D'] / war_pivot['All']], 
                             axis = 1)
-war_pivot_calc.reset_index(inplace = True)
-war_pivot_calc.columns = ['Team',
+war_df.reset_index(inplace = True)
+war_df.columns = ['Team',
                          'Season',
                          'D_WAR',
                          'F_WAR',
@@ -36,49 +38,51 @@ war_pivot_calc.columns = ['Team',
                          'D_Pct']
 
 # Import team Expected Goals totals by season
-file_names = ['Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-2.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-3.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-4.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-5.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-6.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-7.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-8.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-9.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-10.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-11.csv',
-             'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-12.csv']
-file_list = []
+xgoals_files = ['Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-2.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-3.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-4.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-5.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-6.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-7.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-8.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-9.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-10.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-11.csv',
+               'Evolving_Hockey_standard_team_stats_All_no_adj_2019-05-01-12.csv']
+xgoals_data = []
 
-for filename in file_names:
+for filename in xgoals_files:
     df = pd.read_csv(filename, index_col = None, header = 0)
-    file_list.append(df)
+    xgoals_data.append(df)
 
-teams_seasons = pd.concat(file_list, axis = 0, ignore_index = True)
+xgoals_df = pd.concat(xgoals_data, axis = 0, ignore_index = True)
 
 # Create list of unique seasons
-seasons_list = teams_seasons['season'].unique().tolist()
+seasons_list = xgoals_df['season'].unique().tolist()
 
-### JOIN WAR_PIVOT_CALC AND TEAMS_SEASONS xG_diff ON TEAM AND SEASON 
+# Merge war_df and xgoals_df
+stats_df1 = pd.merge(war_df[['Team', 'Season', 'All_WAR', 'D_Pct']], 
+                     xgoals_df[['Team', 'season', 'G_diff', 'xG_diff']], 
+                     left_on = ['Team', 'Season'], 
+                     right_on = ['Team', 'season'])
 
 # Scrape standings
 
 import requests
 
-hf_url = [
-        'https://www.hockey-reference.com/leagues/NHL_2019_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2018_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2017_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2016_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2015_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2014_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2013_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2012_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2011_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2010_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2009_standings.html',
-        'https://www.hockey-reference.com/leagues/NHL_2008_standings.html'
-        ]
+hf_url = ['https://www.hockey-reference.com/leagues/NHL_2019_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2018_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2017_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2016_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2015_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2014_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2013_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2012_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2011_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2010_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2009_standings.html',
+         'https://www.hockey-reference.com/leagues/NHL_2008_standings.html']
 
 response = requests.get('https://www.hockey-reference.com/leagues/NHL_2019_standings.html')
 content = response.content
